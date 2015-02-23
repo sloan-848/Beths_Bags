95880c5ef0e022062e1de411ef63036c354cd8f0
Will Sloan <sloan.848@osu.edu>
2015-02-23 05:46:50 +0000

    Initial commit to Github

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/getOrders', function(req, res) {
+  if(req.header('Owner') != 'Will'){
+    res.send('Invalid request.');
+  } else {
+    var db = req.db;
+    var collection = db.get('orders');
+    collection.find({},{},function(e,docs){
+      res.type('json');
+      res.jsonp(docs);
+    });
+  }
+});
+
+router.post('/submit', function(req,res){
+  var db = req.db;
+  var collection = db.get('orders');
+  var name = req.body.name;
+  var email = req.body.email;
+  var message = req.body.message;
+  if (name && email && message){
+    collection.insert({ "name" : name, "email" : email, "message" : message});
+    res.send('Success!');
+  } else {
+    res.send('Your submission failed.');
+  }
+});
+module.exports = router;
